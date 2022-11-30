8e0d489dc3ab9a622f3f94d5e58a9c2e99f0ac84
Melpositivity <emeldahamatengo@gmail.com>
2022-11-30 19:32:58 +0300

    task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            str = "FizzBuzz"
+        elif num % 5 == 0:
+            str = "Buzz"
+        elif num % 3 == 0:
+            str = "Fizz"
+        else:
+            str = num
+        print("{} ".format(str), end="")
