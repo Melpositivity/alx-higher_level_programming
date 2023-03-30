31263d5de1d05a08224caad1af4719b53a426af8
Melpositivity <emeldahamatengo@gmail.com>
2023-03-31 00:20:17 +0300

    advanced tasks

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+# Send request to respond with the specific message
+curl -sL 0.0.0.0:5000/catch_me_2 -X PUT -H "Origin: School" -d "user_id=98"
