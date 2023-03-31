6f478419902c68e3007ac4087e2e412a69713ee1
Melpositivity <emeldahamatengo@gmail.com>
2023-03-31 11:27:27 +0300

    resubmission

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
-curl -s "$1" | wc -c
+#displays the size of the body of the response
+curl -s -o /dev/null -w "%{size_download}" "$1"
