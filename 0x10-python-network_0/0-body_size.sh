f3f21ef93c5e0c147bab7262d239092531a440f0
Melpositivity <emeldahamatengo@gmail.com>
2023-04-03 19:30:53 +0300

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-#displays the size of the body of the response
-curl -s -o /dev/null -w "%{size_download}" "$1"
+
+# Check if URL is provided as an argument
+if [ $# -eq 0 ]; then
+  echo "Please provide a URL"
+  exit 1
+fi
+
+# Send request using curl and store the response body in a variable
+response=$(curl -s -w '%{size_download}' -o /dev/null "$1")
+
+# Display the size of the response body in bytes
+echo "Response body size: $response bytes"
+
